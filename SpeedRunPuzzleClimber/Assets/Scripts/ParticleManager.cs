6ee7293c410630720b1fb5f7068dc865f6515ee8
zzzack77 <125263916+zzzack77@users.noreply.github.com>
2026-09-05 21:34:04 +0100

    Made the player a prefab and fixed a bug with the particles dispearing somtimes

--- SpeedRunPuzzleClimber/Assets/Scripts/ParticleManager.cs
+++ SpeedRunPuzzleClimber/Assets/Scripts/ParticleManager.cs
@@ -8,6 +8,14 @@ public class ParticleManager : MonoBehaviour
     [SerializeField] private GameObject bodySpark;
 
 
+
+    [SerializeField] private ParticleSystem fireParticle;
+
+    public void CleartAllParticles()
+    {
+        fireParticle.Clear();
+    }
+
     public void InstantiateSparkHalo(Vector3 vector)
     {
         Instantiate(sparkHalo, vector, Quaternion.identity);
