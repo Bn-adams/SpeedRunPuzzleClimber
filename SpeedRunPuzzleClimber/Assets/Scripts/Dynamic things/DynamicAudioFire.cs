18f03dc961a9d4b37500edf9e3e11d9c5e269ac0
zzzack77 <125263916+zzzack77@users.noreply.github.com>
2026-09-06 23:03:16 +0100

    Roaring fire sound effect to when your in dyno mode

--- SpeedRunPuzzleClimber/Assets/Scripts/Dynamic things/DynamicAudioFire.cs
+++ SpeedRunPuzzleClimber/Assets/Scripts/Dynamic things/DynamicAudioFire.cs
@@ -6,11 +6,16 @@ public class DynamicAudioFire : MonoBehaviour
 
     [SerializeField] AudioSource _roaringFire;
 
+    [SerializeField] bool isDyno = false;
+
+
     [SerializeField] float minVolume = 0.1f;
     [SerializeField] float maxVolume = 1f;
 
     [SerializeField] float minPitch = 0.85f;
     [SerializeField] float maxPitch = 1.15f;
+
+
     private void Awake()
     {
         _playerManager = FindAnyObjectByType<PlayerManager>();
@@ -18,7 +23,24 @@ public class DynamicAudioFire : MonoBehaviour
 
     private void Update()
     {
-        _roaringFire.volume = Mathf.Lerp(minVolume, maxVolume, _playerManager.PlayerSpeedScaler);
-        _roaringFire.pitch = Mathf.Lerp(minPitch, maxPitch, _playerManager.PlayerSpeedScaler);
+        if (!isDyno)
+        {
+            _roaringFire.volume = Mathf.Lerp(minVolume, maxVolume, _playerManager.PlayerSpeedScaler);
+            _roaringFire.pitch = Mathf.Lerp(minPitch, maxPitch, _playerManager.PlayerSpeedScaler);
+        }
+        if (isDyno)
+        {
+            if (_playerManager.dyno.dynoed)
+            {
+                _roaringFire.volume = Mathf.Lerp(minVolume, maxVolume, _playerManager.PlayerSpeedScaler);
+                _roaringFire.pitch = Mathf.Lerp(minPitch, maxPitch, _playerManager.PlayerSpeedScaler);
+            }
+            else
+            {
+                _roaringFire.volume = 0;
+
+            }
+        }
+        
     }
 }
