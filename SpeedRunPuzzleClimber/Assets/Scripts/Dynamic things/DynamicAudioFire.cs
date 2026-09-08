e606690d05b0391d6140e02cdc5fa63ca7e97668
zzzack77 <125263916+zzzack77@users.noreply.github.com>
2026-09-08 19:53:38 +0100

    more changes to mouse and keyboard input

--- SpeedRunPuzzleClimber/Assets/Scripts/Dynamic things/DynamicAudioFire.cs
+++ SpeedRunPuzzleClimber/Assets/Scripts/Dynamic things/DynamicAudioFire.cs
@@ -5,6 +5,7 @@ public class DynamicAudioFire : MonoBehaviour
     private PlayerManager _playerManager;
 
     [SerializeField] AudioSource _roaringFire;
+    [SerializeField] GameObject _roaringFireFadeOut;
 
     [SerializeField] bool isDyno = false;
 
@@ -15,7 +16,7 @@ public class DynamicAudioFire : MonoBehaviour
     [SerializeField] float minPitch = 0.85f;
     [SerializeField] float maxPitch = 1.15f;
 
-
+    private float fadeOutSpeed = 0.005f;
     private void Awake()
     {
         _playerManager = FindAnyObjectByType<PlayerManager>();
@@ -35,9 +36,12 @@ public class DynamicAudioFire : MonoBehaviour
                 _roaringFire.volume = Mathf.Lerp(minVolume, maxVolume, _playerManager.PlayerSpeedScaler);
                 _roaringFire.pitch = Mathf.Lerp(minPitch, maxPitch, _playerManager.PlayerSpeedScaler);
             }
-            else
+            else if (_roaringFire.volume > 0)
             {
-                _roaringFire.volume = 0;
+                //GameObject r = Instantiate(_roaringFireFadeOut);
+                //r.GetComponent<AudioSource>().volume = _roaringFire.volume;
+                //r.GetComponent<AudioSource>().pitch = _roaringFire.pitch;
+                _roaringFire.volume -= fadeOutSpeed;
 
             }
         }
